e78e40bad59ee5c3190ac59353d6ff1f4fab56f1
Evandro Passaia <evandrobaraka@gmail.com>
2024-10-01 00:16:48 -0300

    Terminada a parte de leitura do arquivo e exibição dos investimentitos

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import customtkinter as tk
 from utilidades.funcoes import *
 
 janela = tk.CTk()
-janela.geometry('600x560')
+janela.geometry('760x560')
 janela.title('Controle de Criptos')
 janela.grid_columnconfigure((0, 1), weight=1)
 
 
-lista_criptos = nomes_moedas() 
+lista_criptos = nomes_moedas()
+tabela_investimentos = listar_investimentos()
+tabela_em_string = ''
+for linha in tabela_investimentos:
+    tabela_em_string += linha + '\n'
+
+
 
 def exibir_cotacao(moeda_selecionada):
     valor = float(cotar_moeda(moeda_selecionada))
@@ -16,16 +22,19 @@ def exibir_cotacao(moeda_selecionada):
     
     
 # Criar labels e campos
-titulo = tk.CTkLabel(janela, font=('', 20), text='Controle de Criptos')
-label_consulta = tk.CTkLabel(janela, text='Consulta Cripto')
-label_cotacao = tk.CTkLabel(janela, text='Cotação Atual')
-select_moeda = tk.CTkOptionMenu(janela, width=170, values=lista_criptos, command=exibir_cotacao)
-valor_moeda = tk.CTkLabel(janela, text='R$ 0,00')
-label_compra = tk.CTkLabel(janela, text='Compra de Moeda')
+titulo = tk.CTkLabel(janela, font=('', 24), text='Controle de Criptos')
+
+label_consulta = tk.CTkLabel(janela, font=('', 18), text='Consulta Cripto')
+label_cotacao = tk.CTkLabel(janela, font=('', 18), text='Cotação Atual')
+select_moeda = tk.CTkOptionMenu(janela, width=200, font=('', 16), values=lista_criptos, command=exibir_cotacao)
+valor_moeda = tk.CTkLabel(janela, font=('', 16), text='R$ 0,00')
+label_compra = tk.CTkLabel(janela, font=('', 18), text='Compra de Moeda')
 campo_compra = tk.CTkEntry(janela, width=170, placeholder_text='Digite o Valor comprado')
 botao_comprar = tk.CTkButton(janela, height=50, border_width=3, hover=True, font=('', 16), text='Comprar')
-janela_dados_investimentos = tk.CTkScrollableFrame(janela, width=550, height=100)
-label_dados_investimentos = tk.CTkLabel(janela_dados_investimentos, text=ler_arquivo_investimentos(), justify='left')
+
+janela_dados_investimentos = tk.CTkScrollableFrame(janela, width=710, height=100)
+label_dados_investimentos = tk.CTkLabel(janela_dados_investimentos,font=('Courier New', 13), text=tabela_em_string, justify='left')
+
 frame_total_investido = tk.CTkFrame(janela)
 label_total_investido = tk.CTkLabel(frame_total_investido, text='Total Investido')
 frame_lucro_prejuizo = tk.CTkFrame(janela)
@@ -42,7 +51,9 @@ valor_moeda.grid(column=1, row=2, pady=(0, 10), padx=20, sticky='w')
 label_compra.grid(column=0, row=4, pady=(20, 10), padx=20, sticky='w')
 campo_compra.grid(column=0, row=5, pady=(0, 10), padx=20, sticky='w')
 botao_comprar.grid(column=1, row=4, pady=(0, 10), padx=20, sticky='sew', rowspan=2)
-janela_dados_investimentos.grid(column=0, row=6, pady=20, padx=20, columnspan=2)
+janela_dados_investimentos.grid(column=0, row=6, pady=20, padx=10, columnspan=2)
+
+
 label_dados_investimentos.pack(anchor='w')
 frame_total_investido.grid(column=0, row=7, pady=(0,20), padx=20, columnspan=2, sticky='w')
 label_total_investido.pack(pady=10, padx=40)
--- utilidades/funcoes.py
+++ utilidades/funcoes.py
@@ -2,12 +2,6 @@ import pandas as pd
 import requests
 
 
-def ler_arquivo_investimentos():
-    arquivo = pd.read_excel('utilidades/compra_cripto.xlsx')
-    
-    return arquivo
-
-
 def nomes_moedas():
     url = "https://api.binance.com/api/v3/exchangeInfo"
     resposta = requests.get(url)
@@ -41,3 +35,20 @@ def cotar_moeda(moeda):
         return preco_btc
 
 
+def ler_arquivo_investimentos():
+    arquivo = pd.read_excel('utilidades/compra_cripto.xlsx')
+    arquivo['data_transacao'] = pd.to_datetime(arquivo['data_transacao'], errors='coerce')
+    
+    return arquivo
+
+
+def listar_investimentos():
+    tabela = [f"{'Moeda':^5} | {'Transação':^9} | {'Data da Transacao':^15} | {'Cotação na Data':^18} | {'Comprado':^12} | {'Total Comprado':^14}"]
+    arquivo = ler_arquivo_investimentos()
+    
+    for i, row in arquivo.iterrows():
+        data_formatada = row['data_transacao'].strftime('%d/%m/%Y') if pd.notnull(row['data_transacao']) else 'N/A'
+        tabela.append(f"{row['moeda']:^5} | {row['transacao']:^11} | {data_formatada:^15} | R${row['cotacao_na_data']:^16} | R${row['comprado']:^10.2f} | {row['total_comprado']:^14.10f}")
+        
+    return tabela
+        
