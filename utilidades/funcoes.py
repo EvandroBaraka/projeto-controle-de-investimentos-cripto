c3834274ad9b492c469ddcee49b30cfd69087f8a
Evandro Passaia <evandrobaraka@gmail.com>
2025-05-29 23:24:02 -0300

    Modificada a função de formatar a cotação para que exiba os mais casas decimais quando os valores forem menores que 0.10

--- utilidades/funcoes.py
+++ utilidades/funcoes.py
@@ -25,7 +25,11 @@ def nomes_moedas():
 
 def formatar_cotacao(moeda_selecionada):
     valor = float(cotar_moeda(moeda_selecionada))
-    return f'R$ {valor:.2f}'
+    print(valor)
+    if(valor >= 0.10):
+        return f'R$ {valor:.2f}'
+    else:
+        return f'R$ {valor:.8f}'
 
 
 def cotar_moeda(moeda):
